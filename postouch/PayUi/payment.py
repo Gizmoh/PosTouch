618c05031599b57ca0d5db42eeca22fc36a7a8b5
Arturo <Caesarisnotdead@gmail.com>
2013-12-30 19:04:44 -0300

    Added Warning and initial verification function

--- postouch/PayUi/payment.py
+++ postouch/PayUi/payment.py
@@ -8,6 +8,9 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 
+class Warning(ModalView):
+	pass
+
 class Selector(Accordion):
 	subtotal = ObjectProperty(None)
 	efectivo = ObjectProperty(None)
@@ -18,6 +21,7 @@ class Payment(BoxLayout):
 	Total = 0
 	def __init__(self,parent = None,**kwargs):
 		super(Payment, self).__init__(**kwargs)
+		self.alert = Warning()
 		self.ScreenRoot = parent
 		print(self.ScreenRoot)
 		self.Select = Selector()
@@ -25,4 +29,17 @@ class Payment(BoxLayout):
 		self.Select.efectivo.bind(on_text_validate = self.CalcChange)
 
 	def CalcChange(instance,self):
-		print(int(instance.Total)-int(instance.Select.efectivo.text))
+		#print(format_price(int(instance.Total)-int(instance.Select.efectivo.text)))
+		print(str(instance.Total).isdigit())
+		instance.alert.open()
+
+def format_float(n, decimals=1):#Le da formato a los strings con dinero
+    """float -> string"""
+    t = {",": ".", ".": ","}
+    return "".join(t.get(c, c) for c in format(n, ",.{}f".format(decimals)))
+
+def format_price(p, sym="$", decimals=0):
+    """int -> string"""
+    if sym == "CL":
+        sym = "$"
+    return u"{0} {1}".format(sym, format_float(p, decimals=decimals))
